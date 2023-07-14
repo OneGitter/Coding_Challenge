4e2fc822de8e3cf90d0a8c42a4e91a9e7f789c1d
OneGitter <75745398+OneGitter@users.noreply.github.com>
2023-07-14 18:44:44 +0530

    Time: 3 ms (46.81%) | Memory: 6.1 MB (15.08%) - LeetSync

--- leetcode/power-of-two/power-of-two.cpp
+++ leetcode/power-of-two/power-of-two.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         int i = 0;
-        long x = 1;
+        long x = pow(2,i);
 
 
         while(n>=x){
-            if(x==n){
-                return true;
-            }
-            x *= 2;
+            if(x==n) return true;
+            x = pow(2,i++);
         }
 
 
