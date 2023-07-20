203fb6c7d49d66aef433e75e6c26eb926792e524
OneGitter <75745398+OneGitter@users.noreply.github.com>
2023-07-20 13:05:53 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (36.50%) - LeetSync

--- leetcode/70-climbing-stairs/climbing-stairs.cpp
+++ leetcode/70-climbing-stairs/climbing-stairs.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
 
+    int helper(int n,vector<int>& a){
+        if(n<=0) return 0;
+        if(a[n]!=INT_MAX) return a[n];
 
+        a[n] = helper(n-1,a) + helper(n-2,a);
+
+        return a[n];
+    }
 
     int climbStairs(int n) {
-
-        if(n<2) return n;
-        int a =0,b=1,c;
-
-        while(n--){
-            c = a+b;
-            a=b;
-            b=c;
-
-        }
-
-        return c;
+        if(n<3) return n;
+        vector<int> a(n+1,INT_MAX);
+        a[0]= 0;
+        a[1]= 1;
+        a[2]= 2;
+        return helper(n,a);
     }
 };
