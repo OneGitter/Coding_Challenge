a7ba0e8f12cb9de259cd6301fe1269e8da55bf41
OneGitter <75745398+OneGitter@users.noreply.github.com>
2023-07-20 13:00:06 +0530

    Time: 3 ms (35.36%) | Memory: 5.9 MB (72.74%) - LeetSync

---
+++ leetcode/70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+
+
+
+    int climbStairs(int n) {
+
+        if(n<2) return n;
+        int a =0,b=1,c;
+
+        while(n--){
+            c = a+b;
+            a=b;
+            b=c;
+
+        }
+
+        return c;
+    }
+};
