5575b83ff1fe63a99791b63a7638cd0959e599c4
OneGitter <75745398+OneGitter@users.noreply.github.com>
2023-08-11 12:37:07 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Subset Sums/subset-sums.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> subsetSums(vector<int> arr, int N)
+    {
+        // Write Your Code here
+        vector<int> a;
+        
+        a.push_back(0);
+        
+        
+        for(int i=0;i<N;i++){
+            int n = a.size();
+            for(int j=0;j<n;j++){
+                a.push_back(a[j]+arr[i]);
+            }
+        }
+        
+        
+        return a;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        vector<int> arr(N);
+        for(int i = 0 ; i < N ; i++){
+            cin >> arr[i];
+        }
+        Solution ob;
+        vector<int> ans = ob.subsetSums(arr,N);
+        sort(ans.begin(),ans.end());
+        for(auto sum : ans){
+            cout<< sum<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
