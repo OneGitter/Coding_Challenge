bc4090c3c4a5e37c0e3e6784e44d72e7755b4308
OneGitter <75745398+OneGitter@users.noreply.github.com>
2023-08-11 20:37:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (59.78%) - LeetSync

---
+++ leetcode/46-permutations/permutations.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+    void helper(vector<int>& nums,vector<vector<int>>& ans,int i){
+        if(i>=nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+
+
+        for(int j=i;j<nums.size();j++){
+            swap(nums[i],nums[j]);
+            helper(nums,ans,i+1);
+            swap(nums[i],nums[j]);
+        }
+
+
+    }
+
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+
+
+        helper(nums,ans,0);
+
+
+        return ans;
+    }
+};
