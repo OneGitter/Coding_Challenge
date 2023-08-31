db9756f0acc404e9a9da4a40fadc8e7fd0f86b1e
OneGitter <75745398+OneGitter@users.noreply.github.com>
2023-08-31 14:24:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (32.89%) - LeetSync

---
+++ leetcode/1329-minimum-cost-to-move-chips-to-the-same-position/minimum-cost-to-move-chips-to-the-same-position.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minCostToMoveChips(vector<int>& position) {
+        int odd =0,even=0;
+        int n = position.size();
+
+        for(int i=0;i<n;i++){
+            if(position[i]%2==0) even++;
+            else odd++;
+        }
+
+
+        return min(odd,even);
+    }
+};
