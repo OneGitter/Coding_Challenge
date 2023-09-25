a69a7195fcc3385d51103b02ec5fa2dd3a0c0b15
OneGitter <75745398+OneGitter@users.noreply.github.com>
2023-09-25 16:48:03 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (5.25%) - LeetSync

---
+++ leetcode/1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        int d = (a|b);
+        int count=0;
+        for(int i=0;i<32;i++){
+            if(c&1<<i){
+                if(d&1<<i)continue;
+                else count++;
+            }
+            else{
+                if(d&1<<i){
+                    if(a&1<<i)count++;
+                    if(b&1<<i)count++;
+                }
+            }
+        }
+        return count;
+    }
+};
